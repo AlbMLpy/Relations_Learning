Fixed a bag in calculation error(sqrt)

general_functions.py
@@ -44,9 +44,9 @@ def sqrt_err(coo_tensor, vals, shape, a, b, c):
     result = 0.0
     for item in range(coo_tensor.shape[0]):
         coord = coo_tensor[item]
-        result += np.sqrt((vals[item] - np.sum(
-            a[coord[0], :] * b[coord[1], :] * c[coord[2], :]))**2)        
-    return result
+        result += (vals[item] - np.sum(
+            a[coord[0], :] * b[coord[1], :] * c[coord[2], :]))**2        
+    return np.sqrt(result)
 
 
 @jit(nopython=True) 